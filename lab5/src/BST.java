9d9bb50a70c34adb8e0606a9804946a081218b22
shadyman <m.m.shdmn@gmail.com>
2023-05-21 13:56:31 +0600

    Initial commit

---
+++ lab5/src/BST.java
@@ -0,0 +1,12 @@
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node{
+        private K key;
+        private V val;
+        private Node left, right;
+        public Node(K key, V val){
+            this.key = key;
+            this.val = val;
+        }
+    }
+}
