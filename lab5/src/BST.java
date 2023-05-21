803ca1c64c0d9e7b2002f5f99da2da5d2a8903a9
shadyman <m.m.shdmn@gmail.com>
2023-05-21 14:00:32 +0600

    put and size commit

--- lab5/src/BST.java
+++ lab5/src/BST.java
@@ -1,5 +1,6 @@
 public class BST<K extends Comparable<K>, V> {
     private Node root;
+    private int size;
     private class Node{
         private K key;
         private V val;
@@ -9,8 +10,25 @@ public class BST<K extends Comparable<K>, V> {
             this.val = val;
         }
     }
-    public void put(K key, V val){
+    public void put(K key, V val) {
+        root = put(root, key, val);
+    }
 
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = put(node.left, key, val);
+        else if (cmp > 0)
+            node.right = put(node.right, key, val);
+        else
+            node.val = val;
+
+        return node;
     }
     public V get(K key){
 
