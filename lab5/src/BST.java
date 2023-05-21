a6c394a75493f9b145083729d91e661b3da177e3
shadyman <m.m.shdmn@gmail.com>
2023-05-21 14:01:41 +0600

    delete methods commit

--- lab5/src/BST.java
+++ lab5/src/BST.java
@@ -47,9 +47,47 @@ public class BST<K extends Comparable<K>, V> {
         else
             return node;
     }
-    public void delete(K key){
-
+    public void delete(K key) {
+        root = delete(root, key);
     }
+
+    private Node delete(Node node, K key) {
+        if (node == null)
+            return null;
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0)
+            node.left = delete(node.left, key);
+        else if (cmp > 0)
+            node.right = delete(node.right, key);
+        else {
+            if (node.left == null)
+                return node.right;
+            else if (node.right == null)
+                return node.left;
+            else {
+                Node temp = node;
+                node = min(temp.right);
+                node.right = deleteMin(temp.right);
+                node.left = temp.left;
+            }
+        }
+
+        return node;
+    }
+    private Node min(Node node) {
+        if (node.left == null)
+            return node;
+        return min(node.left);
+    }
+
+    private Node deleteMin(Node node) {
+        if (node.left == null)
+            return node.right;
+        node.left = deleteMin(node.left);
+        return node;
+    }
+
     public Iterable<K> iterator() {
 
     }
