70ea62d42c9b8e4a5f03c1027db6207b86f6e49a
shadyman <m.m.shdmn@gmail.com>
2023-05-21 14:02:15 +0600

    Iterable and inOrderTraversal commits

--- lab5/src/BST.java
+++ lab5/src/BST.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BST<K extends Comparable<K>, V> {
     private Node root;
     private int size;
@@ -89,6 +92,16 @@ public class BST<K extends Comparable<K>, V> {
     }
 
     public Iterable<K> iterator() {
+        List<K> keys = new ArrayList<>();
+        inOrderTraversal(root, keys);
+        return keys;
+    }
 
+    private void inOrderTraversal(Node node, List<K> keys) {
+        if (node != null) {
+            inOrderTraversal(node.left, keys);
+            keys.add(node.key);
+            inOrderTraversal(node.right, keys);
+        }
     }
 }
